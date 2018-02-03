a7612865a4ba50ea58bc2bb885e6d8fa85af660a
Zach <ssb.pub.key.is@ZqH7Mctu/7DNInxuwl12ECjfrAKUX2tBLq1rOldNhg0=.ed25519>
2018-02-03 11:16:11 -0800

    Added Documentation for each Function
    I added short doc strings for each function.  Now that I have these, I
    could also delete the default helper comments---and so I did!

--- minesweeper.js
+++ minesweeper.js
@@ -1,22 +1,29 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
+//default values for board size and # of bombs
 var board = {'cells':[]};
 var size = 6;
-var maxbomb = 33;
+var maxBomb = 33;
 
-function makeBoard(size, maxbomb){
-    for (i = 0; i < size; i++)
-	for (j = 0; j < size; j++)//This means within each i, you'll iterate through a bunch of j's.
+function makeBoard(size, maxBomb){
+//create a new object within the cells array, holding 5 default values.
+    for (i = 0; i < size; i++){
+	for (j = 0; j < size; j++){//This means within each i, you'll iterate through a bunch of j's.
 	    board.cells.push({
 		row: i,
 		col: j,
 		isMine: false,
 		isMarked: false,
 		hidden: true })
-    makeBombs(maxbomb) }
+    makeBombs(maxbomb)
+	}
+    }
+}
 
 
 function makeBombs(maxBomb){
+//Turn a random assortment of cells into mines, by marking isMine to true.
+//The number of mines is defined with variable maxBomb
     var numBombs = 0;
     while (numBombs < maxBomb) {
 	var cell = board.cells[randomize()];
@@ -29,46 +36,46 @@ function makeBombs(maxBomb){
 }
 
 function randomize(){
+//Return a random whole number from 0 to max # of cells
     var max = size * size;
-    return Math.floor(Math.random() * max) }
+    return Math.floor(Math.random() * max)
+}
 
 function startGame () {
+// Make a board, for each non-mine display a number for surrounding mines.
     makeBoard(size, maxbomb)
-    for (var i = 0; i < board.cells.length; i++)
+    for (var i = 0; i < board.cells.length; i++){
         board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
     lib.initBoard()
     document.addEventListener('click',checkForWin)
-    document.addEventListener('contextmenu', checkForWin) }
+    document.addEventListener('contextmenu', checkForWin) 
+    }
+}
 
-// Define this function to look for a win condition:
-//
+function checkForWin () {
+// Looks for a win condition then display 'You win!'
+// Conditions are:
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
-function checkForWin () {
     for (var i = 0; i < board.cells.length; i++){
         if (board.cells[i].isMine && board.cells[i].isMarked == false)
             return;
         else if (board.cells[i].isMine == false  && board.cells[i].hidden)
-            return; }
-        lib.displayMessage('You win!'); }
-  // You can use this function call to declare a winner (once you've
-  // detected that they've won, that is!)
-  //   lib.displayMessage('You win!')
+            return; 
+        lib.displayMessage('You win!'); 
+    }
+}
 
 
-// Define this function to count the number of mines around the cell
-// (there could be as many as 8). You don't have to get the surrounding
-// cells yourself! Just use `lib.getSurroundingCells`: 
-//
-// var surrounding = lib.getSurroundingCells(cell.row, cell.col)
-//
-// It will return cell objects in an array. You should loop through 
-// them, counting the number of times `cell.isMine` is true.
+// This function counts the number of mines around a cell
+// (there could be as many as 8). 
 function countSurroundingMines (cell) {
     var count = 0;
     var surroundingCells = lib.getSurroundingCells(cell.row, cell.col);
-    for (i = 0; i < surroundingCells.length; i++)
+    for (i = 0; i < surroundingCells.length; i++){
         if (surroundingCells[i].isMine == true)
 	    count++;
-    return count; }
+    return count;
+    }
+}
 
